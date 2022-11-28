f4f43a86a1cf8448000e5e3ab8808fd49c92417b
Marco Ferlito <MarcoFerlito95@hotmail.com>
2022-11-28 16:09:34 +0100

    fix

---
+++ ex1/ex1.js
@@ -0,0 +1,7 @@
+// Exercise 1: Create a sum script
+
+// Create a script that outputs the total of 5 + 6 when you run it with Node.js.
+function sum(a,b){
+    return a + b 
+}
+console.log(sum(5,6))
